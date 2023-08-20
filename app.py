b7fca18bc75b005e8910cc376eaad954ac793d51
cai-sper <cailin.sperling@gmail.com>
2023-08-20 13:05:05 -0400

    updated Flask file, renamed html file

--- app.py
+++ app.py
@@ -30,15 +30,6 @@ def welcome():
         # "<a href='/api/v1.0/monthly_comparison'>/api/v1.0/monthly_comparison</a><br/>"
     )
 
-
-# @app.route("/api/v1.0/map", methods=['POST'])
-# def map():
-#     'static/js/logic.js' = request.json
-#     print(content)
-#     return jsonify(content)
-    # return jsonify({'status': 0, 'msg': 'success'})
-# return 'static/js/logic.js'
-
 # Create a route that returns the data from the MongoDB as JSON
 @app.route('/api/v1.0/json_data', methods=['GET'])
 def get_ufo_sightings():
@@ -51,20 +42,12 @@ def get_ufo_sightings():
 # a route where we will display a welcome message via an HTML template
 @app.route("/api/v1.0/yearly_comparison")
 def yearly_comparison():
-    return render_template('index.html')
+    return render_template('yearly.html')
 
-
-
-# @app.route("/api/v1.0/yearly_comparison", methods=['GET'])
-# def yearly_comparison():
-#      return send_file('path_to_external_content.html')
-
-
-# @app.route("/api/v1.0/monthly_comparison", methods=['GET'])
-# def monthly_comparison():
-#      # Replace 'path_to_external_content.html' with the actual path to your HTML/JavaScript file
-#      return send_file('path_to_external_content.html')
-
+# a route where we will display a welcome message via an HTML template
+@app.route("/api/v1.0/map")
+def yearly_comparison():
+    return render_template('map.html')
 
 # Run Flask
 if __name__ == '__main__':
