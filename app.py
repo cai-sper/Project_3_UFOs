784d0699da987dc2ebf1519b9c5f089686a0196e
NestorBaptista <ndbaptistaf@gmail.com>
2023-08-20 11:11:01 -0400

    flask welcome page

--- app.py
+++ app.py
@@ -1,24 +1,51 @@
 # Import the dependencies
-from flask import Flask, jsonify
+from flask import Flask, send_file
 from pymongo import MongoClient
 
-# Flask setup
-app = Flask(__name__)
+#################################################
+# Database Setup
+#################################################
 
-# Configure MongoDB connection
 client = MongoClient(port=27017)
 db = client['ufo_db']
 ufo_sightings = db['ufo_sightings']
 
-# Create a route that returns the data from the MongoDB as JSON
-@app.route('/', methods=['GET'])
+#################################################
+# Flask Setup
+#################################################
 
-def get_ufo_sightings():
+app = Flask(__name__)
 
-    # Query for all results in database
-    results = list(db.ufo_sightings.find({}, {'_id': 0}))
+#################################################
+# Flask Routes
+#################################################
 
-    return jsonify(results)
+@app.route("/")
+def welcome():
+    return(
+         "Welcome to Project 3_UFO!<br/><br/>"
+        "Available Routes:<br/>"
+        "<a href='/api/v1.0/map'>/api/v1.0/map</a><br/>"
+        "<a href='/api/v1.0/yearly_comparison'>/api/v1.0/yearly_comparison</a><br/>"
+        "<a href='/api/v1.0/monthly_comparison'>/api/v1.0/monthly_comparison</a><br/>"
+    )
+
+
+@app.route("/api/v1.0/map", methods=['GET'])
+def map():
+     return send_file('path_to_external_content.html')
+
+
+@app.route("/api/v1.0/yearly_comparison", methods=['GET'])
+def yearly_comparison():
+     return send_file('path_to_external_content.html')
+
+
+@app.route("/api/v1.0/monthly_comparison", methods=['GET'])
+def monthly_comparison():
+     # Replace 'path_to_external_content.html' with the actual path to your HTML/JavaScript file
+     return send_file('path_to_external_content.html')
+
 
 # Run Flask
 if __name__ == '__main__':
